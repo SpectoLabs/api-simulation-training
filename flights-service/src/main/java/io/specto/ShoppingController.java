c66aae11d0cc0699a32bb45130179210056ddce1
Andrew Morgan <andrew.morgan@opencredo.com>
2017-09-20 10:41:30 +0200

    Added shopping basket endpoint for stateful Hoverfly work

---
+++ flights-service/src/main/java/io/specto/ShoppingController.java
@@ -0,0 +1,33 @@
+package io.specto;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+@RestController()
+@RequestMapping("/api/v1/shopping-basket")
+public class ShoppingController {
+
+    private final Set<ShoppingItem> shoppingItemSet = newHashSet();
+    private final Random random = new Random();
+
+    @RequestMapping(method = PUT)
+    @ResponseStatus(OK)
+    public void addToBasket(@RequestBody final ShoppingItem shoppingItem) {
+        shoppingItemSet.add(shoppingItem);
+    }
+
+    @RequestMapping(method = GET)
+    public Set<ShoppingItem> getShoppingBasket() {
+        return shoppingItemSet;
+    }
+}
