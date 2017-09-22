e29f39e004aca3e65ccbbcd4649d5477ca689166
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-09-22 11:57:27 +0100

    Add missing RestTemplate bean

--- flights-service/src/main/java/io/specto/FlightsApplication.java
+++ flights-service/src/main/java/io/specto/FlightsApplication.java
@@ -2,9 +2,16 @@ package io.specto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FlightsApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsApplication.class, args);
